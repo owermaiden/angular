d890a5487ed6c61a542be1f60bda9f0e4308a3ef
owermaiden <ower.maiden@cybertekschool.com>
2023-07-04 00:02:22 -0400

    binding done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TypescriptComponent } from './common/typescript/typescript.component';
+import { BindingComponent } from './binding/binding/binding.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TypescriptComponent
+    TypescriptComponent,
+    BindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/binding/binding/binding.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-binding',
+  templateUrl: './binding.component.html',
+  styleUrls: ['./binding.component.css']
+})
+export class BindingComponent {
+  titles: string = 'this is a string';
+  isDisabled : boolean = false;
+
+  inputString: string = 'This is input';
+  name: string = 'Hello World';
+
+  onClick(){
+    console.log('button clicked');
+    this.isDisabled = true;
+  }
+
+  // onSubmit(value){
+  //   this.inputString = value;
+  // }
+}
