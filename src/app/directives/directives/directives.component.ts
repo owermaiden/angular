2c4d0a234d3888b896e3d3c4d667ce130d9d33cf
owermaiden <ower.maiden@cybertekschool.com>
2023-07-04 10:39:07 -0400

    done

--- src/app/directives/directives/directives.component.ts
+++ src/app/directives/directives/directives.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
   templateUrl: './directives.component.html',
   styleUrls: ['./directives.component.css']
 })
-export class DirectivesComponent {
+export class DirectivesComponent implements OnInit {
   isAuthorized: boolean = true;
   mentors: string[] = ['Ower', 'Matt', 'Sahin'];
 
